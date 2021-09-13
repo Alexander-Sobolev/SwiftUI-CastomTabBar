1ef4f973b54a1c51c6283e52501333666bbfe5b3
Alexander-Sobolev <sobolev.ios.developer@gmail.com>
2021-09-13 12:16:05 +0300

    Add CustomTabBar

--- SwiftUICastomTabBar/ContentView.swift
+++ SwiftUICastomTabBar/ContentView.swift
@@ -8,9 +8,63 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var selected = 0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            
+            Spacer()
+            
+            ZStack {
+                
+                HStack {
+                    Button(action: {
+                        self.selected = 0
+                    }) {
+                        Image("Home")
+                    }.foregroundColor(self.selected == 0 ? .black : .gray)
+                    
+                    Spacer(minLength: 12)
+                    
+                    Button(action: {
+                        self.selected = 1
+                    }) {
+                        Image("Search")
+                    }.foregroundColor(self.selected == 1 ? .black : .gray)
+                    
+                    Spacer().frame(width: 120)
+                    
+                    Button(action: {
+                        self.selected = 2
+                    }) {
+                        Image("Person")
+                    }.foregroundColor(self.selected == 2 ? .black : .gray)
+                    
+                    Spacer(minLength: 12)
+                    
+                    Button(action: {
+                        self.selected = 3
+                    }) {
+                        Image("Menu")
+                    }.foregroundColor(self.selected == 3 ? .black : .gray)
+                }.padding()
+                .padding(.horizontal, 22)
+                .background(Color.white)
+                
+                Button(action: {}
+                ) {
+                    Image("Heart")
+                        .renderingMode(.original)
+                        .padding(18)
+                }
+                .background(Color.yellow)
+                .clipShape(Circle())
+                .offset(y: -32)
+                .shadow(radius: 5
+                )
+            }
+        }.background(Color("Color")).edgesIgnoringSafeArea(.top)
     }
 }
 
