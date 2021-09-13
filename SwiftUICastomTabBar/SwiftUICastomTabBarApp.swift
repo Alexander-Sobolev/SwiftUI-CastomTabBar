6adc890f56a139dd2bc74b2c8bfe1d23d195428f
Alexander-Sobolev <sobolev.ios.developer@gmail.com>
2021-09-13 11:42:21 +0300

    Initial Commit

---
+++ SwiftUICastomTabBar/SwiftUICastomTabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICastomTabBarApp.swift
+//  SwiftUICastomTabBar
+//
+//  Created by Alexander Sobolev on 13.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICastomTabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
